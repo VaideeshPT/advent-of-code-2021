85882360585cc0fce7ff3e775320af2bf1a734a3
VaideeshPT <vaideesh.nov94@gmail.com>
2022-03-20 21:21:02 +0000

    day3_part1

---
+++ day3/main.py
@@ -0,0 +1,27 @@
+with open('input.txt') as data:
+    data = data.readlines()
+    data = [reading.strip() for reading in data]
+    length = len(data[0])
+
+def to_int(value):
+    result = sum([int(number)*(2**index) for index, number in enumerate(value[::-1])])
+    return result
+
+def part1(data):
+    gamma_rate = ''
+    epsilon_rate = ''
+
+    for index in range(length):
+        temp_dict = {'1':0, '0':0}
+        for reading in data:
+            temp_dict[reading[index]] += 1
+        gamma_rate += max(temp_dict, key = temp_dict.get)
+        epsilon_rate += min(temp_dict, key = temp_dict.get)
+    power_consumption = to_int(gamma_rate) * to_int(epsilon_rate)
+
+    return power_consumption
+
+print(part1(data)) #2003336
+
+def part2(data):
+    pass
